dd0848b74505d7b7709f8ec7a8fda28925d33710
steaddy <46634450+steaddy@users.noreply.github.com>
2019-03-19 12:43:33 +0300

    Add files via upload
    Значение аргумента функции по умолчанию, фото noImage

--- js/script.js
+++ js/script.js
@@ -6,8 +6,8 @@ const listItems = [
     {name: 'hat', price: 70},
     ];
 
-const itemHTML = function({name, price}) {
-     return `<div class='item'><h3>${name}</h3><span>${price}</span></div>`;
+const itemHTML = function({name, price, imgURL='img/noImg.png'}) {
+     return `<div class='item'><h3>${name}</h3><span>${price}</span><br><img src="${imgURL}" alt="Our Item"></div>`;
 };
 
 document.querySelector('.goods').innerHTML = listItems.map(itemHTML).join('');
